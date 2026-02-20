89e793bf1b0ff948229ade47d2393c882ffd1915
bchaoss <103650247+bchaoss@users.noreply.github.com>
2026-02-20 09:55:09 +0000

    add TRY_CAST logic

--- dbt_pipeline/models/staging/stg_trash_type.sql
+++ dbt_pipeline/models/staging/stg_trash_type.sql
@@ -11,14 +11,22 @@
     'homes_powered'
 ] %}
 
+WITH cleaned as (
 SELECT
     dumpster_id
     , wheel_id
     , date
     {% for col in numeric_cols_to_cast %}
-    , CAST(REPLACE(REPLACE({{ col }}, ' ', ''), ',', '') AS DECIMAL) AS {{ col }}
-    -- {% if not loop.last %}, {% endif %}
+    , TRY_CAST(REPLACE(REPLACE({{ col }}, ' ', ''), ',', '') AS DECIMAL) AS {{ col }}
     {% endfor %}
     , load_time
 FROM
     {{ ref('stg_trash_unique') }}
+)
+
+SELECT *
+FROM cleaned
+WHERE 1=1
+    {% for col in numeric_cols_to_cast %}
+    and {{ col }} IS NOT NULL
+    {% endfor %}
